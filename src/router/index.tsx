4610aa0c7acf5a48e9d297eefb90c6d94f27a55b
SeawardSalmon6 <rayhanachour@hotmail.com>
2025-04-30 12:18:34 -0300

    fix(about): add page to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,12 +5,13 @@ import {
 } from "react-router";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { About } from "../pages/About";
 
 export const routes = createRoutesFromElements(
   <Route path="/">
     <Route index element={<Home />} />
     <Route path="login" element={<Login />} />
-    {/* <Route path="contact" element={<Contact />} /> */}
+    <Route path="sobre" element={<About />} />
     {/* <Route path="*" element={<NotFound />} /> */}
   </Route>
 );

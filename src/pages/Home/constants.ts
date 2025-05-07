5355bed74db14a585db3edd10d567ab9ff2f5580
Paola Guedes <paolaguedesbacca2002@gmail.com>
2025-05-07 15:16:01 -0300

    feat: add markets.json

--- src/pages/Home/constants.ts
+++ src/pages/Home/constants.ts
@@ -1,61 +1,7 @@
 import { MarketModel } from "~types/models/market/types";
+import MARKETS_JSON from "~assets/json/markets.json";
 
-export const MARKETS: MarketModel[] = [
-  {
-    id: "1",
-    name: "Super Muffato Dahma",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/8f0c9193d81e374c2cb66a0463c55e6cc7f8109c?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "1.7km",
-    price: "Aqui sua lista fica por R$ 103,87",
-  },
-  {
-    id: "2",
-    name: "Pão de Açúcar - Dahma",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/ab3dee75a78cd4e8382da751a2a711f50b742988?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "2.9km",
-    price: "Aqui sua lista fica por R$ 129,03",
-  },
-  {
-    id: "3",
-    name: "Proença Supermercados",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/ab3dee75a78cd4e8382da751a2a711f50b742988?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "1.7km",
-    price: "Aqui sua lista fica por R$ 156,90",
-  },
-  {
-    id: "4",
-    name: "Pão de Açúcar - Dahma",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/ab3dee75a78cd4e8382da751a2a711f50b742988?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "2.9km",
-    price: "Aqui sua lista fica por R$ 129,03",
-  },
-  {
-    id: "5",
-    name: "Super Muffato Dahma",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/8f0c9193d81e374c2cb66a0463c55e6cc7f8109c?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "1.7km",
-    price: "Aqui sua lista fica por R$ 103,87",
-  },
-  {
-    id: "6",
-    name: "Proença Supermercados",
-    image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/ab3dee75a78cd4e8382da751a2a711f50b742988?placeholderIfAbsent=true&apiKey=d83e7b782cab4cabaa5ad67426cc8d88",
-    rating: "4.8",
-    distance: "1.7km",
-    price: "Aqui sua lista fica por R$ 156,90",
-  },
-];
+export const MARKETS: MarketModel[] = MARKETS_JSON;
 
 export const FEATURED_MARKETS = [...MARKETS];
 export const BASIC_BASKET_ITEMS = [
